e1f7af9f3e53f1dd4e55c4935d4348352e591240
boying <d4rkl19ht79@gmail.com>
2024-01-25 12:12:14 -0500

    save changes

---
+++ travel_recommendation.js
@@ -0,0 +1,43 @@
+// let countries = [];
+
+// // fetch('./travel_recommendation_api.json')
+// //     .then((response)=> {
+// //         if(!response.ok) {
+// //             throw new Error(`HTTP error! Status: ${response.status}`);
+// //         }
+// //         return response.json();
+// //     })
+// //     .then((response)=> {
+// //         countries = response;
+// //     })
+
+// // let card_str = `<div class="card my-5 shadow">
+// //                     <img src="./assets/${countries.cities.}" class="card-img-top" alt="...">
+// //                     <div class="card-body">
+// //                         <h5 class="card-title">Card title</h5>
+// //                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+// //                         <a href="#" class="btn btn-primary">Go somewhere</a>
+// //                     </div>
+// //                 </div>`
+
+
+// let strResults = "hello";
+let divResults = document.getElementById('search_results');
+
+// // countries.forEach((country)=> {
+// //     country.cities.forEach((city)=> {
+// //         strResults += `<div class="card my-5 shadow">
+// //             <img src="./assets/${city.imageUrl}" class="card-img-top" alt="${city.imageUrl}">
+// //             <div class="card-body">
+// //                 <h5 class="card-title">${city.name}</h5>
+// //                 <p class="card-text">${city.description}</p>
+// //                 <a href="#" class="btn btn-primary">Go somewhere</a>
+// //             </div>
+// //         </div>` + "\n"
+// //     })
+// // })
+// // console.log(strResults);
+// divResults.innerHTML = "<p class='fs-1'>hello</p>";
+
+console.log("hello!");
+divResults.innerHTML = <h1>HELLO!!!</h1>
