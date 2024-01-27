656f9fa9fb0e4992e15ff01850e8f223ae8e9bae
boying <d4rkl19ht79@gmail.com>
2024-01-26 21:14:51 -0500

    save changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,26 +1,63 @@
-let xhr = new XMLHttpRequest();
-const url = './travel_recommendation_api.json';
-let results = "";
+const txtSearch = document.getElementById('txtSearch');
+const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
+
+btnSearch.addEventListener('click',()=>{
+    search_place(txtSearch.value);
+})
+
+btnClear.addEventListener('click',()=>{
+    txtSearch.value="";
+    txtSearch.focus();
+})
 
 
-xhr.open('GET', url, true);
-xhr.responseType = 'json';
-xhr.onload = ()=> {
-    let countries = xhr.response.countries;
-    let temples = xhr.response.temples;
-    let beaches = xhr.response.beaches;
 
-    countries.forEach(country => {
-        let cities = country.cities;
-        cities.forEach(city => {
-            results += card_result(city);
+function search_place(place) {
+    let xhr = new XMLHttpRequest();
+    const url = './travel_recommendation_api.json';
+    let results = "";
+        
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = ()=> {
+        let countries = xhr.response.countries;
+        let temples = xhr.response.temples;
+        let beaches = xhr.response.beaches;
+
+        countries.forEach(country=>{
+            let cities = country.cities;
+            cities.forEach(city=>{
+                if(city.name.toLowerCase().includes(place.toLowerCase())){
+                    results += card_result(city);
+                }
+            })
         })
-    });
-    div_results = document.getElementById('div_results');
-    div_results.innerHTML = results;
-}
+        // countries.forEach(country => {
+        //     let cities = country.cities;
+        //     cities.forEach(city => {
+        //         results += card_result(city);
+        //     })
+        // });
 
-xhr.send();
+        temples.forEach(temple=>{
+            if(temple.name.toLowerCase().includes(place.toLowerCase())){
+                results += card_result(temple);
+            }
+        });
+
+        beaches.forEach(beach => {
+            if(beach.name.toLowerCase().includes(place.toLowerCase())){
+                results += card_result(beach);
+                }
+        });
+
+        div_results = document.getElementById('div_results');
+        div_results.innerHTML = results;
+    }
+
+    xhr.send();
+}
 
 function card_result(obj){
     return `<div class="card mb-5 w-100">
