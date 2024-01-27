879f3a7874af7332f36a5dc05c16374dedb0e891
boying <d4rkl19ht79@gmail.com>
2024-01-26 20:36:09 -0500

    save changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,110 +1,34 @@
-let countries = {
-    "countries": [
-      {
-        "id": 1,
-        "name": "Australia",
-        "cities": [
-          {
-            "name": "Sydney, Australia",
-            "imageUrl": "aus_sydney.jpg",
-            "description": "A vibrant city known for its iconic landmarks like the Sydney Opera House and Sydney Harbour Bridge."
-          },
-          {
-            "name": "Melbourne, Australia",
-            "imageUrl": "aus_melbourne.jpg",
-            "description": "A cultural hub famous for its art, food, and diverse neighborhoods."
-          }
-        ]
-      },
-      {
-        "id": 2,
-        "name": "Japan",
-        "cities": [
-          {
-            "name": "Tokyo, Japan",
-            "imageUrl": "japan_tokyo.jpg",
-            "description": "A bustling metropolis blending tradition and modernity, famous for its cherry blossoms and rich culture."
-          },
-          {
-            "name": "Kyoto, Japan",
-            "imageUrl": "japan_kyoto.jpg",
-            "description": "Known for its historic temples, gardens, and traditional tea houses."
-          }
-        ]
-      },
-      {
-        "id": 3,
-        "name": "Brazil",
-        "cities": [
-          {
-            "name": "Rio de Janeiro, Brazil",
-            "imageUrl": "brazil_rio_de_janeiro.jpg",
-            "description": "A lively city known for its stunning beaches, vibrant carnival celebrations, and iconic landmarks."
-          },
-          {
-            "name": "São Paulo, Brazil",
-            "imageUrl": "brazil_sao_paulo.jpg",
-            "description": "The financial hub with diverse culture, arts, and a vibrant nightlife."
-          }
-        ]
-      }
-    ],
-    "temples": [
-      {
-        "id": 1,
-        "name": "Angkor Wat, Cambodia",
-        "imageUrl": "temples_angkor_wat.jpg",
-        "description": "A UNESCO World Heritage site and the largest religious monument in the world."
-      },
-      {
-        "id": 2,
-        "name": "Taj Mahal, India",
-        "imageUrl": "temples_taj-mahal.jpg",
-        "description": "An iconic symbol of love and a masterpiece of Mughal architecture."
-      }
-    ],
-    "beaches": [
-      {
-        "id": 1,
-        "name": "Bora Bora, French Polynesia",
-        "imageUrl": "beaches_bora_bora.jpg",
-        "description": "An island known for its stunning turquoise waters and luxurious overwater bungalows."
-      },
-      {
-        "id": 2,
-        "name": "Copacabana Beach, Brazil",
-        "imageUrl": "beaches_copacobana.jpg",
-        "description": "A famous beach in Rio de Janeiro, Brazil, with a vibrant atmosphere and scenic views."
-      }
-    ]
-  };
-  
-
-// fetch('./travel_recommendation_api.json')
-//     .then((response)=> {
-//         if(!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//         return response.json();
-//     })
-//     .then((response)=> {
-//         countries = response;
-//     })
+let xhr = new XMLHttpRequest();
+const url = './travel_recommendation_api.json';
+let results = "";
 
 
-let divResults = document.getElementById('search_results');
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+xhr.onload = ()=> {
+    let countries = xhr.response.countries;
+    let temples = xhr.response.temples;
+    let beaches = xhr.response.beaches;
 
-// countries.forEach((country)=> {
-//     country.cities.forEach((city)=> {
-//         strResults += `<div class="card my-5 shadow">
-//             <img src="./assets/${city.imageUrl}" class="card-img-top" alt="${city.imageUrl}">
-//             <div class="card-body">
-//                 <h5 class="card-title">${city.name}</h5>
-//                 <p class="card-text">${city.description}</p>
-//                 <a href="#" class="btn btn-primary">Go somewhere</a>
-//             </div>
-//         </div>` + "\n"
-//     })
-// })
+    countries.forEach(country => {
+        let cities = country.cities;
+        cities.forEach(city => {
+            results += card_result(city);
+        })
+    });
+    div_results = document.getElementById('div_results');
+    div_results.innerHTML = results;
+}
 
-divResults.innerHTML = countries;
+xhr.send();
+
+function card_result(obj){
+    return `<div class="card mb-5 w-100">
+        <img src="./assets/${obj.imageUrl}" class="card-img-top" alt="${obj.imageUrl}">
+        <div class="card-body">
+            <h5 class="card-title">${obj.name}</h5>
+            <p class="card-text">${obj.description}</p>
+            <a href="#" class="btn btn-primary">Book Now</a>
+        </div>
+    </div>`
+}
