cb970125751580090360038310a7c17a07d9050b
boying <d4rkl1ght79@gmail.com>
2024-01-27 14:38:35 +0800

    save changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,19 +1,59 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', './travel_recommendation_api.json');
+xhr.responseType = "json";
+let places = [];
+let slideshow = "";
+
+xhr.onload = () => {
+
+  let countries = xhr.response.countries;
+  let beaches = xhr.response.beaches;
+  let temples = xhr.response.temples;
+  let div_results = document.getElementById("div_results");
+
+    countries.forEach(country=>{
+        country.cities.forEach(city=>{
+            places.push(city);
+        });
+    });
+
+    beaches.forEach(beach=>{
+        places.push(beach);
+    })
+
+    temples.forEach(temple=>{
+        places.push(temple);
+    })
+
+    div_results.innerHTML = card_result(places[0]);
+
+    let places_length = places.length;
+    let place_index = 1;
+
+    slideshow = setInterval(()=>{
+        if(place_index == places_length) place_index = 0;
+        div_results.innerHTML = card_result(places[place_index]);
+        place_index += 1;
+    },5000)
+};
+
+xhr.send();
+
 const txtSearch = document.getElementById('txtSearch');
 const btnSearch = document.getElementById('btnSearch');
 const btnClear = document.getElementById('btnClear');
 
 btnSearch.addEventListener('click',()=>{
     search_place(txtSearch.value);
-})
+});
 
 btnClear.addEventListener('click',()=>{
     txtSearch.value="";
     txtSearch.focus();
-})
-
-
+});
 
 function search_place(place) {
+    clearInterval(slideshow);
     let xhr = new XMLHttpRequest();
     const url = './travel_recommendation_api.json';
     let results = "";
@@ -32,13 +72,7 @@ function search_place(place) {
                     results += card_result(city);
                 }
             })
-        })
-        // countries.forEach(country => {
-        //     let cities = country.cities;
-        //     cities.forEach(city => {
-        //         results += card_result(city);
-        //     })
-        // });
+        });
 
         temples.forEach(temple=>{
             if(temple.name.toLowerCase().includes(place.toLowerCase())){
